f6f68a299eb8a097d230a0b939159fcfdd8174b0
chongyang <89305141@qq.com>
2017-11-02 17:45:53 +0800

    上出图片

--- src/router/dingdan/dingdanAdd.vue
+++ src/router/dingdan/dingdanAdd.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+  	<input :id="uploaderFiles.selectId" type="hidden" />
     <el-form :inline="true" :model="params" class="demo-ruleForm" label-width="100px">
       <h3>订单基本信息</h3>
 
@@ -55,8 +56,8 @@
       <!--<el-table-column  type="selection" width="50"></el-table-column>-->
       <el-table-column label="名称" width="150">
         <template slot-scope="scope">
-          <el-select v-model="scope.row.name" placeholder="单长边外倒45度" @change = "changePrice(priceList,scope.row.name,scope.$index)">
-            <el-option v-for="item in priceList" :key="item.processName" :label="item.name" :value="item.processName"></el-option>
+          <el-select v-model="scope.row.name" placeholder="选择名称" @change = "changePrice(priceList,scope.row.name,scope.$index)">
+            <el-option v-for="item in priceList" :key="item.processName" :label="item.processName" :value="item.processName"></el-option>
           </el-select>
         </template>
       </el-table-column>
@@ -104,13 +105,29 @@
       </el-table-column>
       <el-table-column label="加工示意图" width="200">
         <template slot-scope="scope">
-	        	<uc-upload :uploaderFilesObj="uploaderFilesObj" ref="uploadfile"></uc-upload>
+	        	<div class='uc-upload'>
+					    <div :id="uploaderFiles.dropId" :class="uploaderFiles.showTip ? 'error' : ''">
+					      <span @click="uploade(1,scope.$index)">选择文件</span>
+					    </div>
+					    <span v-show="uploaderFiles.showTip">{{uploaderFiles.tips}}</span>
+					    <section>
+					      <div v-for="(item, index) in scope.row.picture">
+					        <i class="fa fa-times" aria-hidden="true" @click="removeFile(1,scope.$index,index)"></i>
+					        <img :src="item" @click="scaleImg(2, item)">
+					        <div :style="item.style"></div>
+					      </div>
+					    </section>
+					    <footer v-if="scale.show">
+					      <div @click="scaleImg('1')"></div>
+					      <img :src="scale.src">
+					    </footer>
+					  </div>
         </template>
       </el-table-column>
 
       <el-table-column label="操作" >
         <template slot-scope="scope">
-          <el-button type="danger" @click="delTab1(scope.$index)">删除</el-button>
+          <el-button type="danger" v-if="scope.$index!=0" @click="delTab1(scope.$index)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -151,13 +168,29 @@
 
       <el-table-column label="加工示意图" width="200">
        <template slot-scope="scope">
-	        	<uc-upload :uploaderFilesObj="aaa" ref="uploadfile"></uc-upload>
+       		<div class='uc-upload'>
+					    <div :id="uploaderFiles.dropId" :class="uploaderFiles.showTip ? 'error' : ''">
+					      <span @click="uploade(2,scope.$index)">选择文件</span>
+					    </div>
+					    <span v-show="uploaderFiles.showTip">{{uploaderFiles.tips}}</span>
+					    <section>
+					      <div v-for="(item, index) in scope.row.picture">
+					        <i class="fa fa-times" aria-hidden="true" @click="removeFile(2,scope.$index,index)"></i>
+					        <img :src="item" @click="scaleImg(2, item)">
+					        <div :style="item.style"></div>
+					      </div>
+					    </section>
+					    <footer v-if="scale.show">
+					      <div @click="scaleImg('1')"></div>
+					      <img :src="scale.src">
+					    </footer>
+					  </div>
         </template>
       </el-table-column>
 
       <el-table-column label="操作">
         <template slot-scope="scope">
-          <el-button type="danger" @click="delTab2(scope.$index)">删除</el-button>
+          <el-button type="danger" v-if="scope.$index!=0"  @click="delTab2(scope.$index)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -171,7 +204,7 @@
 <script>
   import {CONSTANT} from '../../util/constant';
   import Upload from '~packages/form/upload.vue';
-  import { _Getpricelist,_addDingdanForm} from '../../util/ajax';
+  import { _Getpricelist,_addDingdanForm,_getQiniuUptoken} from '../../util/ajax';
   export default {
     data() {
       return {
@@ -188,19 +221,13 @@
             sizeType:""          //规格类别
           }
         },
-        aaa: {//上传图片
-          label: '上传图片',
-          required: true,
-          selectId: 'qiniu_uploader',
-          dropId: 'qiniu_container',
-          total: 9999999999,
-          mimeTypes: [{title: 'Image files', extensions: 'jpg, jpeg, gif, png'}],
-          multiSelection: false,
-          files: [],
-          showTip: false,
-          tips: '请选择上传图片'
+        uploader: {},
+        uptoken: '',
+        scale: {
+          show: false,
+          src: require('../../images/default.png')
         },
-        uploaderFilesObj: {//上传图片
+        uploaderFiles: {//上传图片
 		          label: '上传图片',
 		          required: true,
 		          selectId: 'qiniu_uploader',
@@ -228,6 +255,8 @@
           routineInfo:'',               //表格1里边的数据
           customInfo:'',                //表格2里边的数据
         },
+        type:0,//区分是基本 还是特殊 加工需求
+        num:0,//插入表的 索引
         //表格数据
 
         tableData1: [
@@ -247,10 +276,10 @@
 
         tableData2:[
           {
-            name:"名称",                  //名称
-            rawSizeType:"2",                //原材料规格:1=600*600、2=800*800、3=600*900、4=600*1200
-            rawNumber:"4",                  //原材料片数，单位：片
-            remark:"备注",                   //备注
+            name:"",                  //名称
+            rawSizeType:"",                //原材料规格:1=600*600、2=800*800、3=600*900、4=600*1200
+            rawNumber:"",                  //原材料片数，单位：片
+            remark:"",                   //备注
             picture:[]                       //图片
           }
         ],
@@ -261,6 +290,11 @@
       UcUpload: Upload
     },
     methods: {
+    	uploade(type,num){
+    		this.type=type;
+    		this.num=num;
+    		document.getElementById(this.uploaderFiles.selectId).click();
+    	},
       removeArr(){//删除元素
       	this.tableData1=this.multipleSelection;
       	console.log(this.tableData1);
@@ -385,10 +419,166 @@
       //格式化日期
       dateChange(val) {
         this.params.basicInfo.processDeadline = val;
+      },
+      initQiniu () {
+        var _this = this;
+
+        _this.uploader = window.Qiniu.uploader({
+          runtimes: 'html5,flash,html4',
+          browse_button: _this.uploaderFiles.selectId,
+          container: _this.uploaderFiles.dropId,
+          drop_element: _this.uploaderFiles.dropId,
+          max_file_size: '100mb',
+          flash_swf_url: 'bower_components/plupload/js/Moxie.swf',
+          dragdrop: true,
+          chunk_size: '4mb',
+          multi_selection: _this.uploaderFiles.multiSelection,
+          uptoken: _this.uptoken,
+          domain: 'ouvvk5b6m.bkt.clouddn.com',
+          get_new_uptoken: false,
+          auto_start: true,
+          log_level: 5,
+          filters: {
+            prevent_duplicates: false,
+            // Specify what files to browse for
+            mime_types: _this.uploaderFiles.mimeTypes || []
+          },
+          init: {
+            'BeforeChunkUpload': function (up, file) {
+              console.log(1);
+            },
+            'FilesAdded': function (up, files) {
+              /*
+              * 当文件数量超出的时候清空uploader.files;并提示
+              * */
+            	 _this.uploaderFiles.files=[];
+              var arr = new Array(..._this.uploaderFiles.files);
+
+
+              files.forEach(function (element) {
+                if (arr.indexOf(element.id) === -1) {
+                  arr.push(element);
+                }
+              });
+              if (arr.length > _this.uploaderFiles.total) {
+                _this.uploaderFiles.showTip = true;
+                _this.uploaderFiles.tips = '上传的文件数量不得超过' + _this.uploaderFiles.total;
+                up.files.splice(_this.uploaderFiles.total);
+                return;
+              }
+              _this.uploaderFiles.showTip = false;
+
+
+              /*
+              * 生成预览图
+              * */
+              files.forEach(function (element) {
+                if (_this.uploaderFiles.files.indexOf(element.id) === -1) {
+                  _this.$set(element, 'style', 'height: 100%');
+                  _this.$set(element, 'url', require('../../images/default.png'));
+                  _this.uploaderFiles.files.push(element);
+                }
+              });
+            },
+            'BeforeUpload': function (up, file) {
+              console.log(1);
+            },
+            'UploadProgress': function (up, file) {
+              for (let item of _this.uploaderFiles.files) {
+                if (item.id === file.id) {
+                  item.style = 'height:' + (100 - file.percent) + '%';
+                }
+              }
+            },
+            'UploadComplete': function () {
+              console.log(1);
+            },
+            'FileUploaded': function (up, file, info) {
+              let domain = up.getOption('domain');
+              let res = JSON.parse(info);
+              let url = 'http://' + domain + '/' + res.key;
+              let localLink = url + '?imageView2/1/w/100/h/100';
+
+              // 上传类型如果的图片就更换已经上传好的图片
+              for (let item of _this.uploaderFiles.files) {
+                if (item.id === file.id) {
+                  item.url = localLink;
+                  if(_this.type==1){
+                  	_this.tableData1[_this.num].picture.push(_this.uploaderFiles.files[0].url);
+                  }else{
+                  	_this.tableData2[_this.num].picture.push(_this.uploaderFiles.files[0].url);
+                  }
+                  
+                }
+              }
+              console.log(_this.uploaderFiles.files[0].url);
+              
+            },
+            'Error': function (up, err, errTip) {
+
+              up.removeFile(err.file.id);
+            }
+          }
+        });
+
+      },
+      getUptoken () {
+        var _this = this;
+
+        _getQiniuUptoken().then(function (res) {
+          var data = res.data;
+
+          if (data.status) {
+            _this.uptoken = data.data.qiniuToken;
+          }
+        });
+      },
+      removeFile (type,num,index) {
+//    	alert(type);
+//    	alert(num);
+//    	alert(index);
+      	if(type==1){
+      		//删除基本加工信息
+      		this.tableData1[num].picture.splice(index, 1)
+      	}else{
+      		//删除特殊加工信息
+      		this.tableData2[num].picture.splice(index, 1)
+      	}
+//      if (fileId) {
+//        this.uploader.removeFile(fileId);
+//      }
+//      this.uploaderFiles.files.splice(index, 1);
+//      this.uploaderFiles.showTip = false;
+      },
+      scaleImg (type, src) {
+        if (type === '1') {
+          this.scale.show = false;
+        } else {
+          this.scale.show = true;
+          this.scale.src = src.split('?imageView2')[0];
+        }
       }
+      
     },
+    
     mounted (){
+    	this.getUptoken();
         this.getPriceList(this.priceParams);
+    },
+    watch: {
+      'uptoken' (newVal) {
+        if (newVal) {
+          this.initQiniu();
+        }
+      },
+      'uploaderFilesObj': {
+        handler: function (newVal) {
+          if (newVal) {
+            this.uploaderFiles = newVal;
+          }
+        },
+        deep: true
+      }
     }
   }
 </script>
@@ -403,5 +593,121 @@
   	text-align: right;
   	background: #62B9FF;
   	padding-right: 130px;
+  }
+  .uc-upload{
+    >label{
+      vertical-align: middle;
+      font-size: 14px;
+      color: #48576a;
+      line-height: 1;
+      box-sizing: border-box;
+      float: none;
+      display: inline-block;
+      text-align: left;
+      padding: 0 0 10px;
+      &.required:before{
+        content: '*';
+        color: #ff4949;
+        margin-right: 4px;
+      }
+    }
+    >div{
+      position: relative;
+      background-color: #fff;
+      border-radius: 4px;
+      border: 1px solid #bfcbd9;
+      color: #1f2d3d;
+      height: 28px;
+      line-height: 28px;
+      padding: 3px 10px;
+      transition: border-color .2s cubic-bezier(.645,.045,.355,1);
+      /*margin: 0 0 22px;*/
+      >span{
+        z-index: 1;
+        border-radius: 4px;
+        border: 1px solid #bfcbd9;
+        padding: 2px 5px;
+        cursor: pointer;
+      }
+      &.error{
+        border-color: red;
+      }
+    }
+    >span{
+      margin-top: -22px;
+      display: block;
+      color: red;
+      height: 22px;
+      line-height: 22px;
+      font-size: 12px;
+    }
+    >section{
+      overflow: hidden;
+      >div{
+        position: relative;
+        width: 100px;
+        height: 100px;
+        float: left;
+        margin: 0 10px 10px 0;
+        >i{
+          position: absolute;
+          top: 0;
+          right: 0;
+          background: black;
+          color: white;
+          font-size: 22px;
+          padding: 2px;
+          border-radius: 50%;
+          height: 26px;
+          width: 26px;
+          text-align: center;
+          opacity: .7;
+          cursor: pointer;
+        }
+        >img{
+          width: 100px;
+          height: 100px;
+        }
+        >div{
+          height: 0%;
+          position: absolute;
+          width: 100%;
+          bottom: 0;
+          left: 0;
+          background: black;
+          opacity: .2;
+          transition: all 0.5s;
+        }
+      }
+    }
+    >footer{
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      z-index: 100;
+      >div{
+        position: absolute;
+        display: block;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        background: black;
+        z-index: 120;
+        opacity: .4;
+      }
+      >img{
+        z-index: 121;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform-origin: 0 0;
+        max-width: 90%;
+        max-height: 90%;
+        transform: translateX(-50%) translateY(-50%);
+      }
+    }
   }
 </style>
