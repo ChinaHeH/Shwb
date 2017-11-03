ed9ac30df634122f648aff5b35a59bf6c7767f7d
Tianche Chen <2392023773@qq.com>
2017-11-03 13:48:48 +0800

    bug

--- src/router/accounts/accountsOrder.vue
+++ src/router/accounts/accountsOrder.vue
@@ -61,7 +61,7 @@
 						￥{{scope.row.orderSum}}
 					</template>
 				</el-table-column>
-				<el-table-column label="操作" width="80">
+				<el-table-column label="操作">
 					<template scope="scope">
 						<el-button size="small" type="primary" @click="detail(scope.row.id)">查看</el-button>
 					</template>
